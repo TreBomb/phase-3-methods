bcd1d47a0a372942ec143d190d3026040a500f2b
Trestin <trestinishak@gmail.com>
2021-09-20 13:39:06 -0500

    finished

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+def add (n1, n2)
+    return n1 + n2
+end
+
+def halve (num)
+    if num.class != Integer
+        return nil
+    else 
+        return num / 2
+    end
+end
